a312bf5d76521fe9930084f06e5b9a33194867f0
Mona Ahmed <mona_ahmed_94@hotmail.com>
2016-02-09 12:27:52 -0800

    now we can sign up but it only checks the username and password

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,16 +1,16 @@
 class AccountsController < ApplicationController
   before_action :set_account, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :ensure_login, only: [:new, :create]
   # GET /accounts
   # GET /accounts.json
-  def index
-    @accounts = Account.all
-  end
+  #def index
+  #  @accounts = Account.all
+  #end
 
   # GET /accounts/1
   # GET /accounts/1.json
-  def show
-  end
+  #def show
+  #end
 
   # GET /accounts/new
   def new
@@ -18,48 +18,50 @@ class AccountsController < ApplicationController
   end
 
   # GET /accounts/1/edit
-  def edit
-  end
+  #def edit
+  #end
 
   # POST /accounts
   # POST /accounts.json
   def create
     @account = Account.new(account_params)
-
-    respond_to do |format|
-      if @account.save
-        format.html { redirect_to @account, notice: 'Account was successfully created.' }
-        format.json { render :show, status: :created, location: @account }
-      else
-        format.html { render :new }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
+    @account.password = params[:account][:password]
+if (  (!Account.find_by(user_name: @account.user_name) ) )
+      if  @account.save
+        redirect_to login_path, notice: 'Account was successfully created.'
+        else
+        render :new , alert: 'Wrong user name/ password combination'
       end
-    end
+else
+        render :new , alert: 'Wrong user name/ password combination'
+
+end
+
   end
 
   # PATCH/PUT /accounts/1
   # PATCH/PUT /accounts/1.json
-  def update
-    respond_to do |format|
-      if @account.update(account_params)
-        format.html { redirect_to @account, notice: 'Account was successfully updated.' }
-        format.json { render :show, status: :ok, location: @account }
-      else
-        format.html { render :edit }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #def update
+  #  respond_to do |format|
+  #    if @account.update(account_params)
+  #      format.html { redirect_to @account, notice: 'Account was successfully updated.' }
+  #      format.json { render :show, status: :ok, location: @account }
+  #    else
+  #      format.html { render :edit }
+  #      format.json { render json: @account.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   # DELETE /accounts/1
   # DELETE /accounts/1.json
-  def destroy
-    @account.destroy
-    respond_to do |format|
-      format.html { redirect_to accounts_url, notice: 'Account was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  #def destroy
+  #  @account.destroy
+  #  respond_to do |format|
+  #    format.html { redirect_to accounts_url, notice: 'Account was successfully destroyed.' }
+  #    format.json { head :no_content }
+  #  end
+  #end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -69,6 +71,6 @@ class AccountsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def account_params
-      params.require(:account).permit(:user_name, :first_name, :last_name, :email, :password_digest)
+      params.require(:account).permit(:user_name, :first_name, :last_name, :email)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :accounts
+  resources :accounts, only: [:new, :create]
 
   resources :pdf_files, only: [:index, :new, :create, :destroy]
   root "pdf_files#index"
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   get "/login" => "sessions#new", as: "login"
   delete "/logout" => "sessions#destroy", as: "logout"
+  get "/signup" => "accounts#new", as: "signup"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
