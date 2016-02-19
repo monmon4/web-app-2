cec3bce44a25995599fd6457b349f896af402436
AmiraNidal <amiranidal3@gmail.com>
2016-02-19 10:21:24 -0800

    final

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,16 +3,16 @@ class NotesController < ApplicationController
 	def create
 		@note= @pdf_file.notes.new(note_params)
 		if @note.save
-			redirect_to @pdf_file,
+			redirect_to pdf_file_note_path({id: note_parent[:pdf_file_id], pdf_file_id: comment_parent[:pdf_file_id]}),
 			 notice: "comment successfully added!"
 		else
-			redirect_to @pdf_files, alert:"failed to add comment!"
+			redirect_to pdf_files_path , alert:"failed to add comment!"
 		end
 	end
 
 	private
 	def set_pdf_file
-		@pdf_file=Pdf_file.find(params[:pdf_file_id])
+		@pdf_file=PdfFile.find(params[:pdf_file_id])
 	end
 	def note_params
 		params.require(:note).permit(:note)
