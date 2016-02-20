2c303001bf07d28f13073b04a4999c403a454802
Mona Ahmed <mona_ahmed_94@hotmail.com>
2016-02-20 15:14:53 -0800

    back to normal with the rename column migration

--- db/migrate/20160212044648_rename_pdf_file_id_to_pdf_file_id.rb
+++ db/migrate/20160212044648_rename_pdf_file_id_to_pdf_file_id.rb
@@ -1,5 +1,5 @@
 class RenamePdfFileIdToPdfFileId < ActiveRecord::Migration
   def change
-  	#rename_column :slides, :PdfFile_id, :pdf_file_id
+  	rename_column :slides, :PdfFile_id, :pdf_file_id
   end
 end
