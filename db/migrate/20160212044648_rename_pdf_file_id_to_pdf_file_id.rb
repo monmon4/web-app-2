9545e215d54175f5562a956bf2c224e96739ba01
Mona Ahmed <mona_ahmed_94@hotmail.com>
2016-02-20 14:51:53 -0800

    heroku migration

--- db/migrate/20160212044648_rename_pdf_file_id_to_pdf_file_id.rb
+++ db/migrate/20160212044648_rename_pdf_file_id_to_pdf_file_id.rb
@@ -1,5 +1,5 @@
 class RenamePdfFileIdToPdfFileId < ActiveRecord::Migration
   def change
-  	rename_column :slide, :PdfFile_id, :pdf_file_id
+  	rename_column :PdfFile, :PdfFile_id, :pdf_file_id
   end
 end
